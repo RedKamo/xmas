38f55ea6d200f57259bcb827e41b823cf82dbdb4
Camilo Suarez <camiloandressuarez1@gmail.com>
2022-11-07 18:24:24 -0500

    :bug: footer bg --- landing/ xmas

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className=" bg-footer-back  text-center  ">
-      <p className="text-lg text-white font-bold backdrop-grayscale-0 bg-black-custom/60 border-2 py-10 border-dark-black ">
+    <footer className=" bg-footer-back text-center py-10">
+      <p className="text-lg text-white font-bold bg-black-custom border-2   border-dark-black ">
         Developed by Camilo Suarez 👨🏾‍💻
       </p>
     </footer>
